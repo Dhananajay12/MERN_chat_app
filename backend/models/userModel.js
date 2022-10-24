c367fdb1fd4a23954937250e8d4afbb50f6831c7
Dhananjay12 <dhananjayavare786@gmail.com>
2022-10-25 00:32:10 +0530

    first commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, reuired: true },
+    email: { type: String, reuired: true },
+    password: { type: String, reuired: true },
+    pic: {
+      type: String,
+      reuired: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestaps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
   const fetchChats = async () => {
-    const data = await axios.get("/api/chat");
-    console.log(data);
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
   };
 
   useEffect(() => {
     fetchChats();
   }, []);
 
-  return <div>ChatPage</div>;
+  return (
+    <>
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </>
+  );
 };
 
 export default ChatPage;
