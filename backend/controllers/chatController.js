c0aac80048e93f140bced5f88fc3c70321a902b3
Dhananjay12 <dhananjayavare786@gmail.com>
2022-10-26 20:42:28 +0530

    first  commit

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -34,11 +34,11 @@ const accessChat = asyncHandler(async (req, res) => {
 
     try {
       const createdChat = await Chat.create(chatData);
-      const FullCHat = await Chat.findOne({ _id: createdChat.id }).populate(
+      const fullChat = await Chat.findOne({ _id: createdChat.id }).populate(
         "users",
         "-password"
       );
-      res.status(200).send(FullCHat);
+      res.status(200).send(fullChat);
     } catch (error) {
       res.status(400);
       throw new Error(error.message);
@@ -46,4 +46,56 @@ const accessChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat };
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("users", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (result) => {
+        const results = await User.populate(result, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const createGroupChat = asyncHandler(async (req, res) => {
+  console.log(req.body.users, req.body.name);
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "please Fill all the feilds" });
+  }
+
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send("More than 2 users are required to form a group chat");
+  }
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+    const fullGroupChat = await Chat.findOne({
+      _id: groupChat.id,
+    })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {}
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const { models } = require("mongoose");
 const { middlewares } = require("../middlewares/authMiddleware");
 const { remove } = require("../models/userModel");
-const { accessChat } = require("../controllers/chatController");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+} = require("../controllers/chatController");
 const router = express.Router();
 
 router.route("/").post(middlewares, accessChat);
-// router.route("/").get(middlewares,fetchChats);
-// router.route("/group").post(middlewares, createGroupChat);
+router.route("/").get(middlewares, fetchChats);
+router.route("/group").post(middlewares, createGroupChat);
 // router.route("/rename").put(middlewares, renameGroup);
 // router.route("/groupremove").put(middlewares, removeFormGroup);
 // router.route("/groupadd").put(middlewares, addToGroup);
